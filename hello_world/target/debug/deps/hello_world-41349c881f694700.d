700c720616a3a498065ada8e5de17fda1fdb9ee0
Brad Roehrig'git config --global user.email bradroehrig@yahoo.comgit config --global --listgit config --global user.name Brad <63978952+bradroehrig@users.noreply.github.com>
2023-05-24 11:13:34 -0500

    commit

---
+++ hello_world/target/debug/deps/hello_world-41349c881f694700.d
@@ -0,0 +1,5 @@
+C:\Users\bradr\Documents\GitHub\Rust_folder\hello_world\target\debug\deps\hello_world-41349c881f694700.rmeta: src\main.rs
+
+C:\Users\bradr\Documents\GitHub\Rust_folder\hello_world\target\debug\deps\hello_world-41349c881f694700.d: src\main.rs
+
+src\main.rs:
---
+++ hello_world/target/debug/deps/hello_world-c5e859dfb42257cc.d
@@ -0,0 +1,5 @@
+C:\Users\bradr\Documents\GitHub\Rust_folder\hello_world\target\debug\deps\hello_world-c5e859dfb42257cc.rmeta: src\main.rs
+
+C:\Users\bradr\Documents\GitHub\Rust_folder\hello_world\target\debug\deps\hello_world-c5e859dfb42257cc.d: src\main.rs
+
+src\main.rs:
