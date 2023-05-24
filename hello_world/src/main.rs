8a1a384e8fa1e796f7594996b42aa3e3f9d5bad2
Brad Roehrig'git config --global user.email bradroehrig@yahoo.comgit config --global --listgit config --global user.name Brad <63978952+bradroehrig@users.noreply.github.com>
2023-05-24 13:35:11 -0500

    commit

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+fn main() {    
+    let numb: i32 =  5;
+    let num2: i32 = 10;
+    let sume = num1 + num2;
+
+
+
+    // print!("{}", variable_name);
 }
---
+++ hello_world/src/tempCodeRunnerFile.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let num1: i32 =  5;
+    let num2: i32 = 10;
+    let sum: i32 = num1 + num2;
+
+    println!("{}", sum)
+
+}
