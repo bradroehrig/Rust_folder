24c6b41a4f5b8a11cad44d8cc7c923465229ca39
Brad Roehrig <63978952+bradroehrig@users.noreply.github.com>
2023-05-24 11:11:53 -0500

    Delete main.rs

--- main.rs
+++
@@ -1,8 +0,0 @@
-// Online Rust compiler to run Rust program online
-// Print "Hello World!" message
-
-fn main() {
-    println!("Hello World!");
-    println!("Hello Brad!");
-    println!("Bradley")
-}
